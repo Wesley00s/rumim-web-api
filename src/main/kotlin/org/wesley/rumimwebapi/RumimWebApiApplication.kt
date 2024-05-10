abd26336c234aa34c0bf6ca5edfed2cff3cd3b25
Wesley R. S <wesley300rodrigues@gmail.com>
2024-05-09 22:52:39 -0300

    Initial commit

---
+++ src/main/kotlin/org/wesley/rumimwebapi/RumimWebApiApplication.kt
@@ -0,0 +1,11 @@
+package org.wesley.rumimwebapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RumimWebApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RumimWebApiApplication>(*args)
+}
---
+++ src/test/kotlin/org/wesley/rumimwebapi/RumimWebApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.wesley.rumimwebapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RumimWebApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
